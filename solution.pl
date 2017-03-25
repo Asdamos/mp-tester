1b9c260373f02d88545c9b13b9eaa38ba7d58cf2
Paweł Smolak <smolak.paul@gmail.com>
2017-03-25 22:56:26 +0100

    Popraw szablon rozwiązania

--- solution.pl
+++ solution.pl
@@ -1,6 +1,6 @@
 % Moduł zawierający predykat solve/2
 
-:- module(solve, [solve/2, op(200, fx, ~), op(500, xfy, v)]).
+:- module(imie_nazwisko, [solve/2, op(200, fx, ~), op(500, xfy, v)]).
 
 % solve(+Input, -Solution)
 % tutaj umieść swoje rozwiązanie
